019fa24c5af34f2d15ccfc592f5ff697a29b8767
Kafka39 <kafka.0039@gmail.com>
2024-11-09 22:56:00 +0500

    Create Консольный вывод, первая поПЫТКА.cs

---
+++ Консольный вывод, первая поПЫТКА.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 10;
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Так-с, это у нас C#. Переменная: {0}", x);
+            Console.WriteLine($"Так-с, это у нас C#. Переменная: {x}");
+            Console.ReadLine();
+        }
+    }
+}
