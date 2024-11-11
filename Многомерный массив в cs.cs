b6847316f4225c8e9cf0e5034bb19216870fcc8b
Kafka39 <kafka.0039@gmail.com>
2024-11-12 00:42:34 +0500

    Create Многомерный массив в cs.cs

---
+++ Многомерный массив в cs.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding= Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            int[,] arr = { 
+
+                { 1, 2, 3 }, 
+                { 4,5,6 } 
+
+            };
+
+
+            Console.WriteLine(arr[4, 5]);
+
+            Console.ReadLine();
+        }
+    }
+}
