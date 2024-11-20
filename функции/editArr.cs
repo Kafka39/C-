870557f67a0c8c495a897da3b744a12fd2680d89
Kafka39 <kafka.0039@gmail.com>
2024-11-20 18:25:59 +0500

    Create editArr.cs

---
+++ функции/editArr.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            int[] arr = { 3, 5, 6 };
+            editArr(arr, 0, 5);
+            Console.WriteLine(arr[0]);
+
+            Console.ReadLine();
+        }
+
+        static void editArr(int[] arr, int index, int value)
+        {
+            arr[index] = value;
+        }
+    }
+}
